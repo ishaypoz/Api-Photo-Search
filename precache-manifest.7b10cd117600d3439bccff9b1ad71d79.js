2adb5266c8fd8feacab77dc718d87d2cee984885
ishaypoz <ishay824@gmail.com>
2020-08-13 13:37:37 +0300

    Updates

---
+++ precache-manifest.7b10cd117600d3439bccff9b1ad71d79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cefdcec73e8716f1b6a423786e99a452",
+    "url": "/Api-Photo-Search/index.html"
+  },
+  {
+    "revision": "1cfd3b071959ff4b80b9",
+    "url": "/Api-Photo-Search/static/css/main.5b03281c.chunk.css"
+  },
+  {
+    "revision": "1a5f38dd9e2f9fe7dfb4",
+    "url": "/Api-Photo-Search/static/js/2.9c98cab2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Api-Photo-Search/static/js/2.9c98cab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cfd3b071959ff4b80b9",
+    "url": "/Api-Photo-Search/static/js/main.1617bee7.chunk.js"
+  },
+  {
+    "revision": "a3685d073eb20552acff",
+    "url": "/Api-Photo-Search/static/js/runtime-main.690f06e0.js"
+  }
+]);
